6998c786c1424f3add625eaea41de6d554f2d631
setrar <setrar@valiha.com>
2020-09-12 16:13:43 -0400

    Create helloas.s

---
+++ apple/helloas.s
@@ -0,0 +1,15 @@
+.data
+    str: .asciz "Hello World\n"
+
+.section    __TEXT,__text
+    .global  _main
+
+_main:
+   pushq   %rbp
+   movq    %rsp, %rbp
+   subq    $32, %rsp
+   leaq    str(%rip), %rdi
+   callq   _printf
+   addq    $32, %rsp
+   popq    %rbp
+   retq
